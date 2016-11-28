4d01f904f306dbae8be68e15b8e063360455c9b9
dyilm <yilmaz.deniz.medi@gmail.com>
2016-11-28 16:45:13 +0100

    NodeJS : ex 9-5 9-6

--- NodeJS/NodeExpr/app.js
+++ NodeJS/NodeExpr/app.js
@@ -1,6 +1,11 @@
 // Includes
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+// DB Init
+mongoose.connect('mongodb://localhost/students');   // Connexion à la base
+var Student = mongoose.model('student', { name: String, first_name: String, email: String });
 
 // App init
 var app = express();
@@ -14,6 +19,18 @@ var server = app.listen(3000,function(){
     //GET
 app.get('/', function (req, res) {
     res.send('Homepage');
+});
+    //POST
+app.post('/', function (req, res) {
+    var bob = new Student({ name: 'bob', first_name : "Sponge", email : "sponge.bob@square.com" });
+    bob.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('meow');
+        res.end();
+      }
+    });
 });
     //PUT
 app.put('/', function (req, res) {
