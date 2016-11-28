a805448a37c4495bc1f672c8cec9d0557eb9710a
dyilm <yilmaz.deniz.medi@gmail.com>
2016-11-28 12:03:22 +0100

    NodeJS : Ex 8 Objets (JSon)

---
+++ NodeJS/ex8-1.js
@@ -0,0 +1,13 @@
+const Ecole = {
+    'eleves': [
+        {
+            'firstname' : 'Han',
+            'nom' : 'Solo'
+        },
+        {
+            'firstname' : 'Chew',
+            'nom' : 'Bacca'
+        }
+    ]
+};
+console.log(Ecole);
---
+++ NodeJS/ex8-3.js
@@ -0,0 +1,24 @@
+const Ecole = {
+    'profs': [
+        {
+            'firstname' : 'Han',
+            'nom' : 'Solo'
+        },
+        {
+            'firstname' : 'Chew',
+            'nom' : 'Bacca'
+        }
+    ],
+    'eleves': [
+        {
+            'firstname' : 'Marty',
+            'nom' : 'McFly'
+        },
+        {
+            'firstname' : 'Doc',
+            'nom' : 'Brown'
+        }
+    ]
+};
+
+console.log(Ecole.eleves[0].firstname);
